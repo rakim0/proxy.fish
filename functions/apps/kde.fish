7767a740f7a1e74c9067a5042530e33e7f52e056
Rakim Middya <103818757+rakim-0@users.noreply.github.com>
2025-03-02 17:52:15 +0530

    feat: add utils/print for pretty printing

---
+++ functions/apps/kde.fish
@@ -0,0 +1,9 @@
+function set_KDE_proxy
+    kwriteconfig5 --file kioslaverc --group 'Proxy Settings' --key 'httpProxy' 'http://172.31.2.4:8080'
+    kwriteconfig5 --file kioslaverc --group 'Proxy Settings' --key 'httpsProxy' 'http://172.31.2.4:8080'
+    kwriteconfig5 --file kioslaverc --group 'Proxy Settings' --key 'ProxyType' '1'
+end
+
+function unset_KDE_proxy
+    kwriteconfig5 --file kioslaverc --group 'Proxy Settings' --key 'ProxyType' '0'
+end
---
+++ functions/utils/print.fish
@@ -0,0 +1,20 @@
+# just a helper function to print
+# argv[1]:  message to print "proxy set successfully!"
+# argv[2]:  what color to use (if set to none then just use normal)
+#           color should be defined in syntax highlighting variables
+#           check the link below for valid variable names:
+#           https://fishshell.com/docs/current/interactive.html#variables-color
+function print
+    set -l message "message is empty"
+    set -l color "normal"
+
+    if test -n "$argv[1]"
+        set message "$argv[1]"
+    end
+
+    if test -n "$argv[2]"
+        set color "$argv[2]"
+    end
+
+    echo (set_color $$color)$message(set_color normal)
+end
